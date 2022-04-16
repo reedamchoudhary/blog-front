3a49d74d746ce07806cf0123444c50f34217d2f6
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2022-04-16 22:15:30 +0530

    hero added to other pages

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import ThemeToggler from "./ThemeToggler";
 import Search from "./Search";
 import CenteredFlex from "./CenteredFlex";
 import { FaHome } from "react-icons/fa";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import * as Path from "../constants/path";
 import { UserContext } from "../App";
@@ -21,17 +21,36 @@ const Header = () => {
       zIndex={"10"}
       backgroundColor={theme ? colors.darkBgColor : colors.lightBgColor}
     >
-      <Box
-        marginTop={"10px"}
-        visibility={window.location.pathname === Path.HOME ? "hidden" : ""}
-      >
-        <FaHome
-          size={"33px"}
+      <Flex>
+        <Box
+          marginTop={"10px"}
+          visibility={window.location.pathname === Path.HOME ? "hidden" : ""}
+        >
+          <FaHome
+            size={"33px"}
+            cursor={"pointer"}
+            color={"white"}
+            onClick={() => navigate(Path.HOME)}
+          />
+        </Box>
+
+        <Box
+          backgroundColor={theme ? colors.lightBgColor : colors.darkBgColor}
+          position={"relative"}
+          top={"-15px"}
+          left={"150px"}
+          padding={"10px 15px"}
+          borderBottomRadius={"10px"}
+          borderTopLeftRadius={"-5px"}
           cursor={"pointer"}
-          color={"white"}
-          onClick={() => navigate(Path.HOME)}
-        />
-      </Box>
+          marginTop={"10px"}
+          letterSpacing={"2px"}
+          color={theme ? "black" : "white"}
+          visibility={window.location.pathname === Path.HOME ? "hidden" : ""}
+        >
+          <Heading size={"sm"}>RHYTHM.DEV</Heading>
+        </Box>
+      </Flex>
       <CenteredFlex padding={"10px"} h={"7%"} justifyContent={"end"}>
         <Search />
         <ThemeToggler />
